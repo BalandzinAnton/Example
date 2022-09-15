0e0fb550a3ba19af37d332cd6a23f2e583890419
Anton <balandzin2602@gmail.com>
2022-09-15 10:48:33 +0300

    new clear

--- Example003/Program.cs
+++ Example003/Program.cs
@@ -1,6 +1,1 @@
-﻿int numberA = new Random().Next(1, 10);
-Console.WriteLine(numberA);
-int numberB = new Random().Next(1, 10);
-Console.WriteLine(numberB);
-int result = numberA + numberB;
-Console.Write(result);
+﻿
